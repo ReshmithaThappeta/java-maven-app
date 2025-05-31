ffe1af5c79a0a77ca7f453b64d6799c6f108aba7
ReshmithaThappeta <thappetareshmitha@gmail.com>
2025-05-31 16:27:26 +0000

    Creating java-maven-app application

---
+++ java-maven-app/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI/CD pipeline!");
+    }
+
+    public String greet() {
+        return "Hello Jenkins";
+    }
+}
+
